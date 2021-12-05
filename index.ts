8fdb6c1eafb5ba50316ee2e2ade0ffdd2fd23029
gurezo <akihiko.kigure@gmail.com>
2021-12-05 17:41:03 +0900

    test commit

--- index.ts
+++ index.ts
@@ -28,14 +28,6 @@ type DirectionMode = "in" | "out";
 
 type GPIOValue = 0 | 1;
 
-function main() {
-  console.log("aaaaa");
-  console.log("aaaaa");
-  console.log("bbbb");
-  console.log("bbbb");
-  console.log("dddd");
-}
-
 interface GPIOChangeEvent {
   readonly value: GPIOValue;
   readonly port: GPIOPort;
